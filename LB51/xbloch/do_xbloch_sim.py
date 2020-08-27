ff0f383775bdfc98d665f2e1824263d0447c7d89
dhigley6 <dhigley@stanford.edu>
2020-08-26 17:03:55 -0700

    Formatting updates, documentation

--- LB51/xbloch/do_xbloch_sim.py
+++ LB51/xbloch/do_xbloch_sim.py
@@ -26,35 +26,89 @@ STIM_LIMITS = (
 SASE_RESULTS_FILE = "LB51/xbloch/results/multipulse_sase.pickle"
 GAUSS_RESULTS_FILE = "LB51/xbloch/results/gauss.pickle"
 
-def simulate_gaussian_case(duration=0.5):
+
+def simulate_gaussian_case(duration: float = 0.5):
+    """Simulate interaction of Gaussian pulses with 3-level-system vs fluence
+
+    Results are saved in GAUSS_RESULTS_FILE as a dictionary with the
+    same format as the dictionary returned by simulate_multipulse_series
+
+    Parameters:
+    -----------
+    duration: float
+        Duration of pulse to simulate (fs)
+    """
     times = sase_sim.TIMES
     E_in_list = [sase_sim._normalize_pulse(times, gauss(times, 0, sigma=duration), 1)]
     summary_result = simulate_multipulse_series(times, E_in_list)
     with open(GAUSS_RESULTS_FILE, "wb") as f:
         pickle.dump(summary_result, f)
 
-def gauss(x, t0, sigma):
-    """Simulate Gaussian pulse with t0 center and sigma width
+
+def gauss(
+    x: np.ndarray, 
+    t0: float, 
+    sigma: float
+) -> np.ndarray:
+    """Calculate Gaussian pulse with t0 center and sigma width
+
+    Parameters:
+    -----------
+    x: 1d np.ndarray
+        Locations to calculate Gaussian intensity at
+    t0: float
+        Location of Gaussian peak
+    sigma: float
+        Width of Gaussian
+    
+    Returns:
+    --------
+    gaussian: 1d np.ndarray
+        Gaussian intensities
     """
-    gaussian = np.exp((-1/2)*((x-t0)/sigma)**2)
-    integral = np.trapz(np.abs(gaussian)**2, x=x)
-    gaussian = gaussian/np.sqrt(integral)
+    gaussian = np.exp((-1 / 2) * ((x - t0) / sigma) ** 2)
+    integral = np.trapz(np.abs(gaussian) ** 2, x=x)
+    gaussian = gaussian / np.sqrt(integral)
     return gaussian
 
+
 def simulate_multipulse_sase_series(n_pulses: int = 4):
+    """Simulate interaction of SASE pulses with 3-level-system vs fluence
+
+    Results are saved in SASE_RESULTS_FILE as a dictionary with the
+    same format as the dictionary returned by simulate_multipulse_series
+
+    Parameters:
+    -----------
+    n_pulses: int
+        Number of pulses to simulate
+    """
     times = sase_sim.TIMES
     E_in_list = [sase_sim.simulate_gaussian()[1] for _ in range(n_pulses)]
     summary_result = simulate_multipulse_series(times, E_in_list)
     with open(SASE_RESULTS_FILE, "wb") as f:
         pickle.dump(summary_result, f)
 
-def simulate_multipulse_series(times, E_in_list):
+
+def simulate_multipulse_series(
+    times: np.ndarray,
+    E_in_list: List[np.ndarray]
+) -> Dict[str, Union[List[float], np.ndarray]:
     """Simulate interaction of pulses with 3-level system vs fluence
 
-    Results are saved in RESULTS_FILE as a dictionary
-        'fluences' key: list[float]
+    Parameters:
+    -----------
+    times: 1d np.ndarray
+        Times of pulses (fs)
+    E_in_list: List[1d np.ndarray]
+        Electric field strengths of pulses (V/m)
+
+    Returns:
+    --------
+    summary_result: Dictionary
+        'fluences' key: List[float]
             Fluences of simulations (J/cm^2)
-        'stim_efficiencies' key: list[float]
+        'stim_efficiencies' key: List[float]
             Efficiency of stimulated inelastic scattering in %
         'phot': 1d np.ndarray
             Photon energies of saved spectra (eV)
@@ -64,10 +118,6 @@ def simulate_multipulse_series(times, E_in_list):
         'summed_transmitted_fields': 2d np.ndarray (complex)
             Transmitted spectral field strengths. Rows are different fluences
             and columns are different photon energies.
-
-    Parameters:
-    -----------
-
     """
     summed_incident_intensities_list = []
     summed_transmitted_intensities_list = []
@@ -105,7 +155,7 @@ def simulate_multipulse_series(times, E_in_list):
 def _get_stim_efficiency(
     phot: np.ndarray,
     intensity_difference: np.ndarray,
-    linear_intensity_difference: np.ndarray,
+    linear_intensity_difference: np.ndarray
 ) -> float:
     """Return stimulated efficiency in %
 
@@ -161,13 +211,19 @@ def _get_summed_result(
         Normalized intensity difference of transmitted and incident pulses
     """
     phot = sim_results[0]["phot"]  # photon energies
-    I_phot_ins = [np.abs(sim_result["E_phot_in"])**2 for sim_result in sim_results]
+    I_phot_ins = [np.abs(sim_result["E_phot_in"]) ** 2 for sim_result in sim_results]
     summed_incident_intensities = np.sum(I_phot_ins, axis=0)
-    I_phot_outs = [np.abs(sim_result["E_phot_out"])**2 for sim_result in sim_results]
+    I_phot_outs = [np.abs(sim_result["E_phot_out"]) ** 2 for sim_result in sim_results]
     summed_transmitted_intensities = np.sum(I_phot_outs, axis=0)
     intensity_difference = (
-        (summed_transmitted_intensities-summed_incident_intensities) / fluence)
-    return phot, summed_incident_intensities, summed_transmitted_intensities, intensity_difference
+        summed_transmitted_intensities - summed_incident_intensities
+    ) / fluence
+    return (
+        phot,
+        summed_incident_intensities,
+        summed_transmitted_intensities,
+        intensity_difference,
+    )
 
 
 def _run_single_pulse_sim(
@@ -211,27 +267,22 @@ def load_multipulse_data() -> Dict[str, Union[List[float], np.ndarray]]:
 
     Returns:
     --------
-    data: dictionary
-        'fluences' key: list[float]
-            Fluences of simulations (J/cm^2)
-        'stim_efficiencies' key: list[float]
-            Efficiency of stimulated inelastic scattering in %
-        'phot': 1d np.ndarray
-            Photon energies of saved spectra (eV)
-        'summed_incident_fields': 2d np.ndarray (complex)
-            Incident field strengths (V/m). Rows are different fluences
-            and columns are different photon energies.
-        'summed_transmitted_fields': 2d np.ndarray (complex)
-            Transmitted field strengths (V/m). Rows are different fluences
-            and columns are different photon energies.
+    data: Dictionary
+        Format is the same as that returned by simulate_multipulse_series
     """
     with open(SASE_RESULTS_FILE, "rb") as f:
         data = pickle.load(f)
     return data
 
+
 def load_gauss_data() -> Dict[str, Union[List[float], np.ndarray]]:
     """Load saved Gaussian pulse simulation results
+
+    Returns:
+    --------
+    data: Dictionary
+        Format is the same as that returned by simulate_multipulse_series
     """
-    with open(GAUSS_RESULTS_FILE, 'rb') as f:
+    with open(GAUSS_RESULTS_FILE, "rb") as f:
         data = pickle.load(f)
     return data
