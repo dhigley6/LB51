7f230c0d075751972baa0ca59d34662dbcb44574
dhigley6 <dhigley@stanford.edu>
2021-05-09 21:34:40 -0700

    Updated quantitative figure

--- LB51/manuscript_plots/quant2.py
+++ LB51/manuscript_plots/quant2.py
@@ -4,7 +4,10 @@
 EXPERIMENT_FILE = 'data/proc/stim_efficiency.pickle'
 
 FLUENCE_MEASUREMENT_ACCURACY = 0.3    # fraction within which fluence was measured
-EFFICIENCY_TO_AMPLIFICATION = 0.01*0.1/1E-8/1E6    #see function at bottom (millions)
+
+SPON_EFFICIENCY = 0.8
+ACCEPTANCE_ANGLE_FRACTION = 1E5
+EFFICIENCY_TO_AMPLIFICATION = SPON_EFFICIENCY*ACCEPTANCE_ANGLE_FRACTION/1E6    # (millions)
 
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -24,16 +27,16 @@ def quant2():
     with open('LB51/xbloch/results/enhanced_25fs.pickle', 'rb') as f:
         sim_results_25fs_enhanced = pickle.load(f)
     f, axs = plt.subplots(2, 2, figsize=(3.37, 4), sharex='col', sharey='row')
-    _short_stim_plot(axs[0, 0], measured, sim_results_5fs, sim_results_5fs_enhanced)
+    _short_stim_plot(axs[0, 0], measured, sim_results_5fs, sim_results_5fs_enhanced, f)
     _long_stim_plot(axs[0, 1], measured, sim_results_25fs, sim_results_25fs_enhanced)
     _short_absorption_plot(axs[1, 0], measured, sim_results_5fs, sim_results_5fs_enhanced)
     _long_absorption_plot(axs[1, 1], measured, sim_results_25fs, sim_results_25fs_enhanced)
     _format(f, axs)
-    plt.savefig("plots/2021_04_25_quant.eps", dpi=600)
-    plt.savefig("plots/2021_04_25_quant.png", dpi=600)
+    plt.savefig("plots/2021_05_08_quant.eps", dpi=600)
+    plt.savefig("plots/2021_05_08_quant.png", dpi=600)
 
-def _short_stim_plot(ax, measured, sim_results_5fs, sim_results_5fs_enhanced):
-    ax.errorbar(
+def _short_stim_plot(ax, measured, sim_results_5fs, sim_results_5fs_enhanced, f):
+    l1 = ax.errorbar(
         measured["short_fluences"],
         measured["short_efficiencies"] * EFFICIENCY_TO_AMPLIFICATION,
         xerr=measured["short_fluences"] * FLUENCE_MEASUREMENT_ACCURACY,
@@ -43,18 +46,19 @@ def _short_stim_plot(ax, measured, sim_results_5fs, sim_results_5fs_enhanced):
         linestyle="",
         marker="o",
     )
-    ax.plot(
+    l2, = ax.plot(
         sim_results_5fs["fluences"] * 1e3,  # convert from J/cm^2 to mJ/cm^2
         np.array(sim_results_5fs["stim_efficiencies"]) * EFFICIENCY_TO_AMPLIFICATION,
         color="tab:blue",
         label="Three Level\nSimulation",
     )
-    ax.plot(
+    l3, = ax.plot(
         sim_results_5fs_enhanced["fluences"] * 1e3,  # convert from J/cm^2 to mJ/cm^2
         np.array(sim_results_5fs_enhanced["stim_efficiencies"]) * EFFICIENCY_TO_AMPLIFICATION,
         color="tab:orange",
         label="Enhancement\nSimulation",
     )
+    f.legend((l2, l3, l1), ('Three Level Simulation', 'Enhancement Simulation', 'Experiment'), 'upper center', ncol=2)
 
 def _long_stim_plot(ax, measured, sim_results_25fs, sim_results_25fs_enhanced):
     ax.errorbar(
@@ -128,13 +132,13 @@ def _format(f, axs):
     #axs[0].set_xlabel('Fluence (mJ/cm$^2$)')
     #axs[0].set_ylabel('Absorption Loss (Percent)')
     f.text(0.52, 0.02, "Fluence (mJ/cm$^2)", ha="center")
-    plt.tight_layout(w_pad=0, h_pad=0.1, rect=(0, 0.03, 1, 1))
+    plt.tight_layout(w_pad=0, h_pad=0.1, rect=(0, 0.03, 0.98, 0.93))
     axs[0, 0].set_xlim((-100, 1600))
     axs[0, 0].set_ylim((-0.1, 0.65))
     axs[0, 1].set_xlim((-300, 10000))
     axs[1, 0].set_ylim((-5, 100))
     # axs[1].set_ylabel("Inelastic Stim.\nScattering Efficiency (%)")
-    axs[1, 0].legend(loc="upper left", frameon=False)
+    #axs[1, 0].legend(loc="upper left", frameon=False)
     axs[0, 0].text(
         0.9,
         0.1,
