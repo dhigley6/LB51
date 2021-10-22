7bd80243879d193d901d04459e531b05dc895bb8
dhigley6 <dhigley@stanford.edu>
2021-10-22 07:16:06 -0700

    Corrected to divide by spontaneous efficiency rather than multiply

--- LB51/manuscript_plots/rixs_gain.py
+++ LB51/manuscript_plots/rixs_gain.py
@@ -8,8 +8,9 @@ FLUENCE_MEASUREMENT_ACCURACY = 0.3  # fraction within which fluence was measured
 SPON_EFFICIENCY = 0.8
 ACCEPTANCE_ANGLE_FRACTION = 1e5
 EFFICIENCY_TO_AMPLIFICATION = (
-    SPON_EFFICIENCY * ACCEPTANCE_ANGLE_FRACTION
+    ACCEPTANCE_ANGLE_FRACTION/SPON_EFFICIENCY
 )
+# starting efficiency for above factor is in %
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -30,44 +31,49 @@ def plot():
         sim_results_25fs_enhanced = pickle.load(f)
     f = plt.figure(figsize=(3.37, 3.37))
     ax = plt.gca()
-    l1 = axs[0].errorbar(
+    l1 = ax.errorbar(
         measured["short_fluences"],
         measured["short_efficiencies"] * EFFICIENCY_TO_AMPLIFICATION,
         xerr=measured["short_fluences"] * FLUENCE_MEASUREMENT_ACCURACY,
         yerr=measured["short_stds"] * EFFICIENCY_TO_AMPLIFICATION,
         color="m",
-        label="Expt.",
+        label="5 fs Expt.",
         linestyle="",
         marker="o",
     )
-    (l2,) = axs[0].plot(
+    (l2,) = ax.plot(
         sim_results_5fs["fluences"] * 1e3,  # convert from J/cm^2 to mJ/cm^2
         np.array(sim_results_5fs["stim_efficiencies"]) * EFFICIENCY_TO_AMPLIFICATION,
         color="m",
-        label="Three Level\nSimulation",
+        label="5 fs Three Level\nSimulation",
     )
-    axs[0].set_xscale('log')
-    axs[0].set_yscale('log')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
 
-    axs[1].errorbar(
+    ax.errorbar(
         measured["long_fluences"],
         measured["long_efficiencies"] * EFFICIENCY_TO_AMPLIFICATION,
         xerr=measured["long_fluences"] * FLUENCE_MEASUREMENT_ACCURACY,
         yerr=measured["long_stds"] * EFFICIENCY_TO_AMPLIFICATION,
         color="g",
-        label="Expt.",
+        label="25 fs Expt.",
         linestyle="",
         marker="o",
     )
-    axs[1].plot(
+    ax.plot(
         sim_results_25fs["fluences"] * 1e3,  # convert from J/cm^2 to mJ/cm^2
         np.array(sim_results_25fs["stim_efficiencies"]) * EFFICIENCY_TO_AMPLIFICATION,
         color="g",
-        label="Three Level\nSimulation",
+        label="25 fs Three Level\nSimulation",
     )
 
-    axs[1].set_xscale('log')
-    axs[1].set_yscale('log')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('Fluence (mJ/cm$^2$)')
+    ax.set_ylabel('Projected Stimulated RIXS Gain')
+    ax.legend(loc='best')
+    ax.set_xlim((10**(-2.5), 10**(4.5)))
+    plt.tight_layout()
 
 def get_measurements():
     with open(EXPERIMENT_FILE, "rb") as f:
